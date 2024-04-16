3bcba86d17485c20dae4eb8212fb79a7327c8220
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-04-16 22:18:14 +0200

    .

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -8,11 +8,6 @@ const handlePending = state => {
   state.error = null;
 };
 
-const handleFulfilled = state => {
-  state.loading = false;
-  state.error = null;
-};
-
 const handleRejected = (state, action) => {
   state.loading = false;
   state.error = action.payload;
@@ -25,19 +20,19 @@ const contactsSlice = createSlice({
     builder
       .addCase(fetchContacts.pending, handlePending)
       .addCase(fetchContacts.fulfilled, (state, action) => {
-        handleFulfilled(state);
+        state.loading = false;
         state.items = action.payload;
       })
       .addCase(fetchContacts.rejected, handleRejected)
       .addCase(addContact.pending, handlePending)
       .addCase(addContact.fulfilled, (state, action) => {
-        handleFulfilled(state);
+        state.loading = false;
         state.items.push(action.payload);
       })
       .addCase(addContact.rejected, handleRejected)
       .addCase(deleteContact.pending, handlePending)
       .addCase(deleteContact.fulfilled, (state, action) => {
-        handleFulfilled(state);
+        state.loading = false;
         state.items = state.items.filter(item => item.id !== action.payload.id);
       })
       .addCase(deleteContact.rejected, handleRejected);
